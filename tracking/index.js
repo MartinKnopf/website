7a67b2e4b465d4191cd50f88bb34d4e77bbf891f
Martin Knopf <legendaryhorsed@gmail.com>
2019-11-02 21:59:51 +0100

    reading count of apps in apps collection

--- tracking/index.js
+++ tracking/index.js
@@ -21,6 +21,16 @@ const routes = {
     } else {
       res.end('-4')
     }
+  },
+
+  "/stats": (req, res) => {
+    const { query } = parse(req.url, true)
+    
+    if(query && query.app) {
+      getStats('apps', query.app, res)
+    } else {
+      res.end('-4')
+    }
   }
 }
 
@@ -44,6 +54,26 @@ let store = (collName, row, res) => {
             res.end('-3')
           }
         });
+      } catch(e) {
+        res.end('-2')
+      }
+    });
+  } catch(e) {
+    res.end('-1')
+  }
+}
+
+let getStats = (collName, app, res) => {
+  try {
+    MongoClient.connect(DBURI, (err, client) => {
+      try {
+        if(err) throw err
+
+        const db = client.db(DBNAME)
+        const collection = db.collection(collName)
+        collection.find({app: app}).count().then((count) => {
+          res.end('' + count)
+        })
       } catch(e) {
         res.end('-2')
       }
